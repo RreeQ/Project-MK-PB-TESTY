0f008002a47f0bf45cafaf6e3b0041441f40bda1
Paweł Boro <pawel.boro@microsoft.wsei.edu.pl>
2019-01-29 18:06:38 +0100

    Revert "Merge branch 'master' of https://github.com/RreeQ/Project-MK-PB-TESTY"
    This reverts commit 43ef9a8a42445683d94248ba64e57452b6cc48fa, reversing
    changes made to 236b923d35a4ac19327d7ba953dfb26d0f6cb21e.

--- SeleniumMK/UnitTest1.cs
+++ SeleniumMK/UnitTest1.cs
@@ -1,7 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Chrome;
 using System;
 using OpenQA.Selenium.Support.UI;
 
@@ -14,93 +13,56 @@ namespace SeleniumTest
         [SetUp]
         public void StartBrowser()
         {
-            _driver = new ChromeDriver();
+            _driver = new FirefoxDriver();
         }
 
         [Test]
-        public void SeleniumMK()
+        public void GoogleTest()
         {
-            _driver.Navigate().GoToUrl("https://www.x-kom.pl/");
             Random rnd = new Random();
             int month = rnd.Next(50, 99999);
-            var imie = "abdul";
-            var lastname = "selenium";
+            _driver.Navigate().GoToUrl("https://pl.pinterest.com/");
+            var email = "test1selenium" + month + "@abuabudabu.com";
             var password = "test1seleniumhaslo!";
-            var email = "testselenium1" + month + "@gmail.com";
-            _driver.FindElement(By.XPath("//*[@id='header']/header/div/div[2]/div/div[4]/div/a/div/i")).Click();
-            _driver.FindElement(By.XPath("/html/body/div[2]/div/section[2]/div[2]/div/a")).Click();
-            var nameTextBox = _driver.FindElement(By.CssSelector("#firstName"));
-            nameTextBox.SendKeys(imie);
-            var lastnameTextBox = _driver.FindElement(By.CssSelector("#lastName"));
-            lastnameTextBox.SendKeys(lastname);
-            var emailTextBox = _driver.FindElement(By.CssSelector("#email"));
+            var age = "22";
+            var emailTextBox = _driver.FindElement(By.XPath("//*[@id='email']"));
             emailTextBox.SendKeys(email);
-            var passwordTextBox = _driver.FindElement(By.CssSelector("#password"));
+            var passwordTextBox = _driver.FindElement(By.XPath("//*[@id='password']"));
             passwordTextBox.SendKeys(password);
-            _driver.FindElement(By.XPath("//*[@id='termsOfUseAcceptation']")).Click();
-            _driver.FindElement(By.XPath("//*[@id='consent-0865a6f2e72120e8f807bbc8365f0793']")).Click();
-            _driver.FindElement(By.XPath("//*[@id='consent-9a785ca7c529d901c6170cc000221a7e']")).Click();
-            
-            _driver.FindElement(By.XPath("//*[@id='registrationForm']/div[6]/div[5]/div/button")).Click();
+            var ageTextBox = _driver.FindElement(By.XPath("//*[@id='age']"));
+            ageTextBox.SendKeys(age + Keys.Enter);
+
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(10))
+.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.XPath("//*[@id='male']"))));
+            _driver.FindElement(By.XPath("//*[@id='male']")).Click();
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(10))
+.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.CssSelector("._r5"))));
+            _driver.FindElement(By.CssSelector("._r5")).Click();
+
+            System.Threading.Thread.Sleep(10000);
+
+            _driver.FindElement(By.CssSelector(".NuxExtensionUpsell__optionalSkip")).Click();
+
             System.Threading.Thread.Sleep(5000);
-            var lookingfor = _driver.FindElement(By.XPath("//*[@id='searchBar']/div[1]/div[1]/input"));
-            lookingfor.SendKeys("myszka");
-            System.Threading.Thread.Sleep(2000);
-            _driver.FindElement(By.XPath("//*[@id='searchBar']/div[1]/div[3]/button/i")).Click();
-            System.Threading.Thread.Sleep(2000);
-            _driver.FindElement(By.XPath("//*[@id='productList']/div[1]/a/img")).Click();
-            System.Threading.Thread.Sleep(2000);
-            _driver.FindElement(By.XPath("//*[@id='app']/div[2]/div[4]/div/div[2]/div[1]/div[2]/div[2]/button/span/span[2]")).Click();
-            System.Threading.Thread.Sleep(4000);
-            _driver.FindElement(By.XPath("//*[@id='basketItems']/div/div/div[4]/span/span")).Text.Contains("45,00 zł");
 
+            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
 
+            _driver.FindElement(By.CssSelector("._r5")).Click();
 
-            //Random rnd = new Random();
-            //int month = rnd.Next(50, 99999);
-            //var email = "test1selenium" + month + "@abuabudabu.com";
-            //var password = "test1seleniumhaslo!";
-            //var age = "22";
-            //var emailTextBox = _driver.FindElement(By.XPath("//*[@id='email']"));
-            //emailTextBox.SendKeys(email);
-            //var passwordTextBox = _driver.FindElement(By.XPath("//*[@id='password']"));
-            //passwordTextBox.SendKeys(password);
-            //var ageTextBox = _driver.FindElement(By.XPath("//*[@id='age']"));
-            //ageTextBox.SendKeys(age + Keys.Enter);
-            //            new WebDriverWait(_driver, TimeSpan.FromSeconds(10))
-            //.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.XPath("//*[@id='male']"))));
-            //            _driver.FindElement(By.XPath("//*[@id='male']")).Click();
+            System.Threading.Thread.Sleep(5000);
 
-            //            System.Threading.Thread.Sleep(2000);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.FindElement(By.CssSelector(".experienceSystemPushOverlay")).Click();
 
+            _driver.FindElement(By.XPath("/html/body/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div/div[3]/div[4]/div/div/div/a/div/div")).Click();
+            string actualvalue = _driver.FindElement(By.XPath("/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div[1]/h4")).Text;
+            Assert.IsTrue(actualvalue.Contains("Testselenium"));
 
-            //            _driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div[2]/button/div")).Click();
-
-
-            //            System.Threading.Thread.Sleep(5000);
-
-            //            _driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/div[3]/div[2]/div/div")).Click();
-
-            //            System.Threading.Thread.Sleep(5000);
-
-            //            _driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[3]/div/div[1]/div[1]/div[1]")).Click();
-            //            _driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[3]/div/div[1]/div[1]/div[2]")).Click();
-            //            _driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[3]/div/div[1]/div[1]/div[3]")).Click();
-            //            _driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[3]/div/div[1]/div[1]/div[4]")).Click();
-            //            _driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[3]/div/div[1]/div[1]/div[5]")).Click();
-
-            //            _driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/div[4]/div[2]/div/button/div")).Click();
-
-            //            System.Threading.Thread.Sleep(5000);
-
-            //            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            //            _driver.FindElement(By.CssSelector(".experienceSystemPushOverlay")).Click();
-
-            //            _driver.FindElement(By.XPath("/html/body/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div/div[3]/div[4]/div/div/div/a/div/div")).Click();
-            //            string actualvalue = _driver.FindElement(By.XPath("/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div[1]/h4")).Text;
-            //            Assert.IsTrue(actualvalue.Contains("Testselenium"));
-
-            //StringAssert.StartsWith("https://pl.pinterest.com/", _driver.Url);
+            StringAssert.StartsWith("https://pl.pinterest.com/", _driver.Url);
             //_driver.FindElement(By.XPath("//*[@id='Password']")).Click();
             //StringAssert.StartsWith("https://chomikuj.pl", _driver.Url);
             //_driver.FindElement(By.XPath("//*[@id='header - items']/div[2]/div[2]/a")).Click();
