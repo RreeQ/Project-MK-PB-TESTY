621dc065c4e3dc8892bb3e24295e72ce2f589015
RreeQ <marcinek877@gmail.com>
2019-01-29 19:41:41 +0100

    Update Test.cs

--- SeleniumMKmacVersion/Test.cs
+++ SeleniumMKmacVersion/Test.cs
@@ -22,50 +22,67 @@ namespace SeleniumMKmacVersion
         [Test]
         public void TestPinterest()
         {
+            _driver.Navigate().GoToUrl("https://www.x-kom.pl/");
             Random rnd = new Random();
             int month = rnd.Next(50, 99999);
-            _driver.Navigate().GoToUrl("https://pl.pinterest.com/");
-            var email = "test1selenium" + month + "@abuabudabu.com";
+            var imie = "abdul";
+            var lastname = "selenium";
             var password = "test1seleniumhaslo!";
-            var age = "22";
-            var emailTextBox = _driver.FindElement(By.XPath("//*[@id='email']"));
+            var email = "testselenium1" + month + "@gmail.com";
+            _driver.FindElement(By.XPath("//*[@id='header']/header/div/div[2]/div/div[4]/div/a/div/i")).Click();
+            _driver.FindElement(By.XPath("/html/body/div[2]/div/section[2]/div[2]/div/a")).Click();
+            var nameTextBox = _driver.FindElement(By.CssSelector("#firstName"));
+            nameTextBox.SendKeys(imie);
+            var lastnameTextBox = _driver.FindElement(By.CssSelector("#lastName"));
+            lastnameTextBox.SendKeys(lastname);
+            var emailTextBox = _driver.FindElement(By.CssSelector("#email"));
             emailTextBox.SendKeys(email);
-            var passwordTextBox = _driver.FindElement(By.XPath("//*[@id='password']"));
-            passwordTextBox.SendKeys(password);
-            var ageTextBox = _driver.FindElement(By.XPath("//*[@id='age']"));
-            ageTextBox.SendKeys(age + Keys.Enter);
 
-            new WebDriverWait(_driver, TimeSpan.FromSeconds(10))
-.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.XPath("//*[@id='male']"))));
-            _driver.FindElement(By.XPath("//*[@id='male']")).Click();
-            new WebDriverWait(_driver, TimeSpan.FromSeconds(10))
-.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.CssSelector("._r5"))));
-            _driver.FindElement(By.CssSelector("._r5")).Click();
 
-            System.Threading.Thread.Sleep(5000);
+            //            Random rnd = new Random();
+            //            int month = rnd.Next(50, 99999);
+            //            _driver.Navigate().GoToUrl("https://pl.pinterest.com/");
+            //            var email = "test1selenium" + month + "@abuabudabu.com";
+            //            var password = "test1seleniumhaslo!";
+            //            var age = "22";
+            //            var emailTextBox = _driver.FindElement(By.XPath("//*[@id='email']"));
+            //            emailTextBox.SendKeys(email);
+            //            var passwordTextBox = _driver.FindElement(By.XPath("//*[@id='password']"));
+            //            passwordTextBox.SendKeys(password);
+            //            var ageTextBox = _driver.FindElement(By.XPath("//*[@id='age']"));
+            //            ageTextBox.SendKeys(age + Keys.Enter);
 
-            _driver.FindElement(By.CssSelector(".NuxExtensionUpsell__optionalSkip")).Click();
+            //            new WebDriverWait(_driver, TimeSpan.FromSeconds(10))
+            //.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.XPath("//*[@id='male']"))));
+            //            _driver.FindElement(By.XPath("//*[@id='male']")).Click();
+            //            new WebDriverWait(_driver, TimeSpan.FromSeconds(10))
+            //.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.CssSelector("._r5"))));
+            //            _driver.FindElement(By.CssSelector("._r5")).Click();
 
-            System.Threading.Thread.Sleep(5000);
+            //            System.Threading.Thread.Sleep(5000);
 
-            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
-            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
-            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
-            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
-            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            //            _driver.FindElement(By.CssSelector(".NuxExtensionUpsell__optionalSkip")).Click();
 
-            _driver.FindElement(By.CssSelector("._r5")).Click();
+            //            System.Threading.Thread.Sleep(5000);
 
-            System.Threading.Thread.Sleep(13000);
+            //            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            //            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            //            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            //            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
+            //            _driver.FindElement(By.CssSelector(".NuxPickerInterestsGrid > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > div:nth-child(1) > button:nth-child(1)")).Click();
 
-            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            _driver.FindElement(By.CssSelector(".experienceSystemPushOverlay")).Click();
+            //            _driver.FindElement(By.CssSelector("._r5")).Click();
 
-            _driver.FindElement(By.XPath("/html/body/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div/div[3]/div[4]/div/div/div/a/div/div")).Click();
-            string actualvalue = _driver.FindElement(By.XPath("/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div[1]/h4")).Text;
-            Assert.IsTrue(actualvalue.Contains("Testselenium"));
+            //            System.Threading.Thread.Sleep(13000);
 
-            StringAssert.StartsWith("https://pl.pinterest.com/", _driver.Url);
+            //            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            //            _driver.FindElement(By.CssSelector(".experienceSystemPushOverlay")).Click();
+
+            //            _driver.FindElement(By.XPath("/html/body/div/div/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/div/div[3]/div[4]/div/div/div/a/div/div")).Click();
+            //            string actualvalue = _driver.FindElement(By.XPath("/html/body/div[2]/div/div[1]/div/div[1]/div[1]/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div[1]/h4")).Text;
+            //            Assert.IsTrue(actualvalue.Contains("Testselenium"));
+
+            //StringAssert.StartsWith("https://pl.pinterest.com/", _driver.Url);
             //_driver.FindElement(By.XPath("//*[@id='Password']")).Click();
             //StringAssert.StartsWith("https://chomikuj.pl", _driver.Url);
             //_driver.FindElement(By.XPath("//*[@id='header - items']/div[2]/div[2]/a")).Click();
